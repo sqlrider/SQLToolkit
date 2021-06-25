bad14aafcfbff81bc884667027e0820dedb5bbb1
Alex Stuart <68782526+sqlrider@users.noreply.github.com>
2021-06-25 12:29:40 +0100

    Update Query Profiler Trace.sql

--- Profiler/Query Profiler Trace.sql
+++ Profiler/Query Profiler Trace.sql
@@ -1,42 +1,42 @@
 /*******************************************************************************************************************
 *** Querying Profiler Trace
 *** 
-*** Load a Profiler trace into a table to query textdata efficiently.
+*** Query/load a Profiler trace into a table to query plans.
+*** Uncomment INTO line and run step-by-step to load into table then drop.
 ***
 *** Ver		Date		Author
 *** 1.0		01/02/18	Alex Stuart
 *** 
 ********************************************************************************************************************/
-
-
-USE [databasename]
+USE DBA
 GO
 
 SELECT *
-INTO dbo.tablename
-FROM fn_trace_gettable('D:\Traces\filename.trc', default)
+-- INTO dbo.TraceData
+FROM fn_trace_gettable('L:\Trace\MonitorUsage.trc', default)
 
-
--- Convert TextData column to VARCHAR so it can be indexed
-ALTER TABLE dbo.tablename
+ALTER TABLE dbo.TraceData
 ALTER COLUMN TextData VARCHAR(MAX)
 
-CREATE NONCLUSTERED INDEX idx_tablename_TextData
-ON dbo.tablename (TextData)
+CREATE CLUSTERED INDEX cix_TraceData_StartTime
+ON dbo.TraceData (StartTime)
+
+CREATE NONCLUSTERED INDEX idx_TraceData_TextData
+ON dbo.TraceData (TextData)
 
 /*
 
 SELECT *
-FROM dbo.tablename
+FROM dbo.TraceData
 WHERE duration > 1000000
 ORDER BY Duration DESC, Reads DESC
 
 SELECT MIN(Duration) AS 'MinDuration', AVG(Duration) AS 'AvgDuration', MAX(Duration) AS 'MaxDuration',
 		MIN(Reads) AS 'MinReads', AVG(Reads) AS 'AvgReads', MAX(Reads) AS 'MaxReads',
 		MIN(CPU) AS 'MinCPU', AVG(CPU) AS 'AvgCPU', MAX(CPU) AS 'MaxCPU'
-FROM dbo.tablename
-WHERE Textdata LIKE 'text you're looking for%'
+FROM dbo.TraceData
+WHERE Textdata LIKE '%filter text here%'
 
 */
 
-DROP TABLE dbo.tablename
+DROP TABLE dbo.TraceData
